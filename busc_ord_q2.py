37ba4c99c98b07215adb86c51428e7881eebcd8d
Breno Marques <132229523+brenomarq@users.noreply.github.com>
2023-10-04 21:27:50 -0300

    Update busc_ord_q2.py

--- busc_ord_q2.py
+++ busc_ord_q2.py
@@ -1,29 +1,65 @@
 # Incompleta
 # Questão 2 - Busca e Ordenação
-def confere_igualdade(lista ,key):
+def confere_peso(objeto, key):
     i = 0
-    comparado = 0
-    for l, elem in enumerate(lista):
-        if l == 0:
-            comparado = elem[key]
-        elif elem[key] == comparado:
+    comparado = ""
+    for j, elem in enumerate(objeto):
+        if j == 0:
+            comparado == elem[key]
+        elif comparado == elem[key]:
             i += 1
-    
-    if i > 0:
-        return False
-    return True
+        else:
+            break
 
-qtd_homens = int(input())
-homens = []
+    return i
 
-for i in range(qtd_homens):
+
+def confere_altura(objeto, key):
+    i = 0
+    comparado = ""
+    for j, elem in enumerate(objeto):
+        if j == 0:
+            comparado == elem[key]
+        elif comparado == elem[key]:
+            i += 1
+
+    return i
+
+
+qtd_pretendentes = int(input())
+pretendentes = []
+
+# Faz a coleta dos dados dos pretendentes
+for _ in range(qtd_pretendentes):
     nome, sobrenome, altura, peso = input().split()
     altura = abs(int(altura) - 180)
-    peso = int(peso)
-    homem = [nome, sobrenome, altura, peso]
-    homens.append(homem)
+    peso = int(peso) - 75
+    pretendente = [nome, sobrenome, altura, peso]
+    pretendentes.append(pretendente)
 
-homens = sorted(homens, key=lambda x: x[2], reverse=True)
+# Ordena os rapazes pela altura
+pretendentes = sorted(pretendentes, key=lambda x: x[2])
 
-if confere_igualdade(homens, 3):
-    print(f"Qualquer coisa")
+# Confere se há pretendentes da mesma altura
+msm_altura = confere_altura(pretendentes, 2)
+
+# Ordena os rapazes baseado no peso
+if msm_altura != 0:
+    lista_aux = pretendentes[:msm_altura+1]
+    lista_aux = sorted(lista_aux, key=lambda x: x[3])
+
+    for i in range(msm_altura):
+        pretendentes[i] = lista_aux[i]
+
+# Confere se há pretendentes com o msm peso
+msm_peso = confere_peso(pretendentes, 3)
+
+# Ordena baseado no primeiro nome
+if msm_peso != 0:
+    lista_aux = pretendentes[:msm_peso+1]
+    lista_aux = sorted(lista_aux, key=lambda x: x[0])
+
+    for i in range(msm_altura):
+        pretendentes[i] = lista_aux[i]
+
+print(pretendentes)
