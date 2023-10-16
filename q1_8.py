a6f66945881f6ba90112396239d372a09a6b3cff
Breno Marques <132229523+brenomarq@users.noreply.github.com>
2023-10-16 16:18:41 -0300

    Create q1_8.py

---
+++ q1_8.py
@@ -0,0 +1,11 @@
+# Introdução - Questão 8
+# Precisa ser finalizado
+qtd_linhas = int(input())
+linhas = list()
+
+for _ in range(qtd_linhas):
+	linha = input()
+	linhas.append(linha)
+
+for linha in linhas:
+	...
