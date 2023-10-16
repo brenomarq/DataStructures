9125c484c975f34c638e1c1a3bea73e300efe1ae
Breno Marques <132229523+brenomarq@users.noreply.github.com>
2023-10-16 09:52:36 -0300

    fixed q1_5.py

--- q1_5.py
+++ q1_5.py
@@ -1,32 +1,25 @@
 # Introdução - Questão 5
-# Precisa de atualização...
 while True:
     try:
         qtd_buracos = int(input())
-        x_buracos = list()
-        y_buracos = list()
+        coordenadas_buracos = list()
+        
+        coordenadas_coelho = tuple(map(float, input().split()))
+        coordenadas_raposa = tuple(map(float, input().split()))
 
-        x_coelho, y_coelho = map(float, input().split())
-        x_raposa, y_raposa = map(float, input().split())
+        for _ in range(qtd_buracos):
+            coordenadas_buraco = tuple(map(float, input().split()))
+            coordenadas_buracos.append(coordenadas_buraco)
 
-        for i in range(qtd_buracos):
-            x_buraco, y_buraco = map(float, input().split())
-            x_buracos.append(x_buraco)
-            y_buracos.append(y_buraco)
+        for buraco in coordenadas_buracos:
+            distancia_coelho = ((coordenadas_coelho[0] - buraco[0])**2 + (coordenadas_coelho[1] - buraco[1])**2)**(0.5)
+            distancia_raposa = ((coordenadas_raposa[0] - buraco[0])**2 + (coordenadas_raposa[1] - buraco[1])**2)**(0.5)
 
-        buracos_possiveis = list()
-        for i in range(qtd_buracos):
-            distancia_coelho = ((x_coelho-x_buracos[i])**2 * (y_coelho-y_buracos[i])**2)*1/2
-            distancia_raposa = ((x_raposa-x_buracos[i])**2 * (y_raposa-y_buracos[i])**2)*1/2
-
-            if distancia_coelho <= distancia_raposa/2:
-                buracos_possiveis.append((x_buracos[i], y_buracos[i]))
-
-        if buracos_possiveis:
-            print(f"O coelho pode escapar pelo buraco ({buracos_possiveis[0][0]:.3f}, {buracos_possiveis[0][1]:.3f}).")
+            if distancia_coelho <= (distancia_raposa/2):
+                print(f"O coelho pode escapar pelo buraco ({buraco[0]:.3f}, {buraco[1]:.3f}).")
+                break
         else:
             print("O coelho nao pode escapar.")
-
 
     except EOFError:
         break
