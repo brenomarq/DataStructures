f4ea8e559532a4a792726e5c754f5534ffd94537
Breno Marques <132229523+brenomarq@users.noreply.github.com>
2023-10-17 17:45:22 -0300

    Update q1_8.py

--- q1_8.py
+++ q1_8.py
@@ -1,5 +1,4 @@
 # Introdução - Questão 8
-# Precisa ser finalizado
 qtd_linhas = int(input())
 linhas = list()
 
@@ -8,4 +7,22 @@ for _ in range(qtd_linhas):
 	linhas.append(linha)
 
 for linha in linhas:
-	...
+    qtd_1 = linha.count("1")
+    conta_1 = 0
+    conta_0 = 0
+
+    if qtd_1 == 0:
+         print(conta_0)
+         continue
+
+    i = 0
+    while linha[i] != "1":
+        i += 1
+    while conta_1 != qtd_1 and i < len(linha):
+        if linha[i] == "1":
+            conta_1 += 1
+        else:
+            conta_0 += 1
+        i += 1
+
+    print(conta_0)
