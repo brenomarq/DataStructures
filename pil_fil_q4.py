f7accfd7647b927c30f5a8864138bd37d501e897
Breno Marques <132229523+brenomarq@users.noreply.github.com>
2023-10-30 22:30:32 -0300

    Create pil_fil_q4.py

---
+++ pil_fil_q4.py
@@ -0,0 +1,21 @@
+# Pilha e fila - Questão 4
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.items:
+            return self.items.pop()
+
+    def peek(self):
+        if self.items:
+            return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
